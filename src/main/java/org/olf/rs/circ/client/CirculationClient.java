5d740cc7a7af4de5b80d5b73a921e1869ecaa74b
Michelle <msuranofsky@gmail.com>
2020-03-30 15:31:59 -0400

    Remove target and fix circ client

--- src/main/java/org/olf/rs/circ/client/CirculationClient.java
+++ src/main/java/org/olf/rs/circ/client/CirculationClient.java
@@ -1,11 +1,18 @@
 package org.olf.rs.circ.client;
 
+import java.io.IOException;
+
+import org.extensiblecatalog.ncip.v2.service.ServiceException;
+import org.extensiblecatalog.ncip.v2.service.ValidationException;
+import org.json.JSONObject;
+
 public interface CirculationClient {
 	
 	void setEndpoint(String endpoint);
 	void setUserid(String userid);
 	void setPassword(String password);
 	void addHttpHeader(String key, String value); 
+	JSONObject send(NCIPCircTransaction service) throws ServiceException, ValidationException, IOException;
 	
 	
 	
