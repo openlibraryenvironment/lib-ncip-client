166e0542b79aae84da0931227312e00c65733133
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2024-01-18 10:27:16 -0500

    Tests for v1 CancelRequestItem

---
+++ lib-ncip-client/src/test/java/org/olf/rs/circ/client/unitTests/transactions/CancelRequestItemTests.java
@@ -0,0 +1,31 @@
+package org.olf.rs.circ.client.unitTests.transactions;
+
+import org.json.JSONObject;
+import org.junit.Test;
+import org.olf.rs.circ.client.CancelRequestItem;
+import org.olf.rs.circ.client.TestConstants;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class CancelRequestItemTests {
+    @Test
+    public void testGenerateNcip1Object() {
+        CancelRequestItem cancelRequestItem = new CancelRequestItem();
+        cancelRequestItem.setRequestId("123");
+        cancelRequestItem.setFromAgency("ABC");
+        cancelRequestItem.setToAgency("CBA");
+        String xml = cancelRequestItem.generateNCIP1Object();
+        assertNotNull(xml);
+    }
+
+    @Test
+    public void testGenerateNcip1Response() {
+        String mockFileName = TestConstants.PATH_TO_MOCK_FILES + "ncipOneCancelRequestItemXmlResponseExample.xml";
+        String xmlAsString = TestUtil.readLineByLine(mockFileName);
+        CancelRequestItem cancelRequestItem = new CancelRequestItem();
+        JSONObject cancelRequestItemResponse = cancelRequestItem.constructResponseNcip1Response(xmlAsString);
+        assertEquals("12345", cancelRequestItemResponse.get("requestId"));
+
+    }
+}
